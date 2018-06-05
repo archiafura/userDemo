ea62b89cb4cee449464e92f0bc1e4c81dcb7c5a1
darkdadis <buquet.regis@hotmail.fr>
2018-06-05 14:22:23 +0200

    reinitialisation du repository

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -3,87 +3,13 @@
 namespace App\Repository;
 
 use App\Entity\Product;
-//use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-use Doctrine\ORM\EntityRepository;
 
-
-
-///**
- //* @method Product|null find($id, $lockMode = null, $lockVersion = null)
- //* @method Product|null findOneBy(array $criteria, array $orderBy = null)
-
- //* @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- //*/
-
-
-
-
-class ProductRepository extends EntityRepository
+class ProductRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $product)
+    public function __construct(RegistryInterface $registry)
     {
-        parent::__construct($product, Product::class);
+        parent::__construct($registry, Product::class);
     }
-//
-//    /**
-//     * @param $products
-//     * @return Product[]
-//     */
-//
-//    public function findAll($products) : array
-//    {
-//        $products = $this->getDoctrine()
-//            ->getRepository(Product::class)
-//            ->findAll();
-//
-//
-//
-//    }
-
-        /*
-        $result = $this->createQueryBuilder('p')
-
-            ->select(array('product'))
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult();
-
-
-       return array("getResult" => $result->getResult(), "getArrayResult" => $result->getArrayResult(),);
-
-        */
-
-        /*
-        $conn = $this->getEntityManager()->getConnection();
-
-        $sql = '
-        SELECT * FROM product p
-        ORDER BY p.id ASC
-        ';
-
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-
-        return $stmt->fetchAll();
-
-
-
-
-
-    }
-*/
-
-    /*
-    public function findOneBySomeField($value): ?Product
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
